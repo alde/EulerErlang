29316a7ad513ebda4316d83594cb2c91f2cb56ea
Rickard Dybeck <r.dybeck@gmail.com>
2011-07-30 17:16:20 +0200

    Added comments to the euler2 file. Rewrote euler1 to support standalone running.

--- euler1.erl
+++ euler1.erl
@@ -1,7 +1,17 @@
 -module(euler1).
--export([result/0]).
+-export([start/1]).
+% If we list all the natural numbers below 10 that are
+% multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of
+% these multiples is 23.
 
-result() -> io:format("Result: ~w~n", [solve(1000)]).
+%Find the sum of all the multiples of 3 or 5 below 1000.
+
+start(Args) when (length(Args) == 1) -> result(1000);
+start(Args) ->
+        Limit = list_to_integer(lists:nth(2, Args)),
+        result(Limit).
+
+result(N) -> io:format("Sum of numbers below ~p divisible by 3 or 5: ~p~n", [N, solve(N)]).
 
 solve(Sum, Cur, N) when Cur >= N -> Sum;
 solve(Sum, Cur, N) when (0 == (Cur rem 3)) or (0 == (Cur rem 5)) -> solve(Sum+Cur, Cur+1, N);
