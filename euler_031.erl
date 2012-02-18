5cb49e1dd0647aaabf764e262b8f17bcc5a42cb9
Rickard Dybeck <r.dybeck@gmail.com>
2012-02-18 12:28:43 +0100

    problem 31
    runtime: 123 ms

---
+++ euler_031.erl
@@ -0,0 +1,25 @@
+-module(euler_031).
+-export([result/0]).
+
+result() ->
+    T1 = now(),
+    Res = solve(200),
+    T2 = now(),
+    io:format("~w (~w ms) ~n", [Res, timer:now_diff(T2,T1)/1000]).
+
+solve (Total) ->
+    Denominations = [200, 100, 50, 20, 10, 5, 2, 1],
+    ways(Total, Denominations).
+
+% If we reach the end, increase by one (combination successful)
+ways(0, _) ->
+    1;
+% If we run out of cons, exit
+ways(_, []) ->
+    0;
+% If we overshoot the target, exit
+ways(N, _) when (N < 0) ->
+    0;
+% Remove the current coin, and recurse into the tail, but first recurse into the
+% current set of coins with the current coin removed.
+ways(N, [Coin|Tail]) -> ways(N, Tail) + ways(N-Coin, [Coin|Tail]).
