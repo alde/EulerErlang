a16d3ba9a8f9ccdd335be86d90b59e05db32ccc1
Rickard Dybeck <r.dybeck@gmail.com>
2012-02-19 10:54:41 +0100

    Documentation

---
+++ README.md
@@ -0,0 +1,4 @@
+[Project Euler][1] problems solved in [Erlang][2]. This is in no way intended as a demonstration of Erlang, it is simply my way to try to learn the language.
+
+[1]: http://projecteuler.net/
+[2]: http://www.erlang.org/
--- euler_031.erl
+++ euler_031.erl
@@ -1,6 +1,15 @@
 -module(euler_031).
 -export([result/0]).
 
+% In England the currency is made up of pound, £, and pence, p, and there are
+% eight coins in general circulation:
+%
+% 1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) and £2 (200p).
+% It is possible to make £2 in the following way:
+%
+% 1 x £1 + 1 x 50p + 2 x 20p + 1 x 5p + 1 x 2p + 3x 1p
+% How many different ways can £2 be made using any number of coins?
+
 result() ->
     T1 = now(),
     Res = solve(200),
